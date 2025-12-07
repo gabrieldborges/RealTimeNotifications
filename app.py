a4b626cad77230940d93bee5974f331a2abc9346
gabrieldborges <gabrieldantas1404@icloud.com>
2025-12-07 20:37:12 -0300

    Basic Payment API Structure so far. Imported libs; created flask app with a working create_payment route; created a payment class and a pix class that generates a working qrCode and stores it in a local database sqlite

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, jsonify, request
+from repository.database import db
+from db_models.payment import Payment
+from datetime import datetime, timedelta
+from payments.pix import Pix
+
+
+
+app = Flask(__name__)   
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'SECRET_KEY_WEBSOCKET'
+db.init_app(app)
+
+
+@app.route('/payments/pix',methods=['POST'])
+def create_payment_pix():
+    data = request.json
+    
+    if 'value' not in data:
+        return jsonify({"message": "Invalid value."}),400
+        
+    expiration_date = datetime.now() + timedelta(minutes=30)   
+    new_payment = Payment(value = data['value'], expiration_date = expiration_date) 
+
+    pix_obj = Pix()
+    data_payment_pix = pix_obj.create_payment()
+    
+    new_payment.bank_payment_id = data_payment_pix['bank_payment_id']
+    new_payment.qr_code = data_payment_pix['qr_code_path']
+    
+    
+    db.session.add(new_payment)
+    db.session.commit()
+    
+    
+    return jsonify({"message": "Payment has been created.",
+                    "payment" : new_payment.to_dict()
+                    })
+
+    
+    
+    
+    
+    
+@app.route('/payments/pix/confirmation',methods=['POST'])
+def confirm_payment_pix():
+    return jsonify({"message": "Payment has been confirmed."})
+
+
+@app.route('/payments/pix/<int:payment_id>',methods=['GET'])
+def get_payment_pix_page(payment_id):
+    return 'Pagamento PIX'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
